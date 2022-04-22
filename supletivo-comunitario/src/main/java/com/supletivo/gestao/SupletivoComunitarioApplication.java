6bdbd831133b23fb153a81c8dabccef05e6215ca
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2022-04-22 15:36:37 -0300

    add projeto

---
+++ supletivo-comunitario/src/main/java/com/supletivo/gestao/SupletivoComunitarioApplication.java
@@ -0,0 +1,13 @@
+package com.supletivo.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupletivoComunitarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupletivoComunitarioApplication.class, args);
+	}
+
+}
---
+++ supletivo-comunitario/src/test/java/com/supletivo/gestao/SupletivoComunitarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supletivo.gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupletivoComunitarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
