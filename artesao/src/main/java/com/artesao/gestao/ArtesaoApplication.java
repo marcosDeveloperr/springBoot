87794761807b21a1396ca6248e9163614c3bcb97
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2022-04-21 22:11:36 -0300

    add projeto artesao

---
+++ artesao/src/main/java/com/artesao/gestao/ArtesaoApplication.java
@@ -0,0 +1,13 @@
+package com.artesao.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtesaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtesaoApplication.class, args);
+	}
+
+}
---
+++ artesao/src/test/java/com/artesao/gestao/ArtesaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artesao.gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtesaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
